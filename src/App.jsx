d01d566f92a20a4e952835152cd06752da2d357c
Nauman-555 <naumannzaib@gmail.com>
2023-12-16 00:33:37 +0500

    Styled

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ const [month, setMonth] = useState(today.getMonth()+1)
 const [day, setDay] = useState(today.getDate())
 const [holidays, setHolidays] = useState(null)
 const [datavar, setDatavar] = useState(false)
-const [sprit, setSprit] = useState("Hey! Let Us See The Holidays")
+const [sprit, setSprit] = useState("Click to Proceed!")
+const [spice, setSpice] = useState("Enter your Data!")
 
 
 const handleClickSearch = () =>{
@@ -25,12 +26,14 @@ const handleClickSearch = () =>{
 const handleClickHome = () =>{
   setDatavar(false)
   setHolidays()
-  setSprit("Hey! Let Us See The Holidays")
+  setSprit("Click to Proceed!")
+  setSpice("Enter Your Data !")
 }
 const handleClickSubmit = () =>{
   setDatavar(true);
   setSprit()
   setHolidays(null);
+  setSpice("Enter Again !");
 
      // Validation for empty fields
      if (!country || !year || !month || !day) {
@@ -69,24 +72,28 @@ const handleClickSubmit = () =>{
 }
 
   return (
-    <div>
-      <h1>{sprit}</h1>
+    <div className='bg'>
+      <h1 className='click'>{sprit}</h1>
     {!datavar ? (
       <button className='but' onClick={handleClickSearch}>Proceed</button>
        ) : (
         
-        <> <h2>Enter Your Choice</h2>
-       <h2>Country</h2> <input type="text" value={country} placeholder="Enter Here" required="required" maxLength={2} onChange={e =>setCountry(e.target.value)}/> <br />
-       <h2>Year</h2><input type="number" value={year} placeholder="Enter Here" maxLength={4} onChange={e =>setYear(e.target.value)}/> <br />
-       <h2>Month</h2> <input type="number" value={month} placeholder="Enter Here" maxLength={2} onChange={e =>setMonth(e.target.value)}/> <br />
-       <h2>Day</h2><input type="number" value={day} placeholder="Enter Here" maxLength={2} onChange={e =>setDay(e.target.value)}/> <br />
+        <> 
+        <div className="bell">
+        <div className='a'><h1>{spice}</h1></div>
+        <div>
+       <h2 className='b'>Country</h2> <input type="text" value={country} placeholder="Enter Here" required="required" maxLength={2} onChange={e =>setCountry(e.target.value)}/> <br />
+       <h2 className='c'>Year</h2><input type="number" value={year} placeholder="Enter Here" maxLength={4} onChange={e =>setYear(e.target.value)}/> <br />
+       <h2 className='d'>Month</h2> <input type="number" value={month} placeholder="Enter Here" maxLength={2} onChange={e =>setMonth(e.target.value)}/> <br />
+       <h2 className='e'>Day</h2><input type="number" value={day} placeholder="Enter Here" maxLength={2} onChange={e =>setDay(e.target.value)}/> <br />
+       </div>
+       </div>
        <button className='butn' onClick={handleClickSubmit}>Submit</button>
         </>)}
         {holidays && Array.isArray(holidays) ? (
   <div>
-    <h2>Holiday Data:</h2>
     {holidays.map((holidayItem, index) => (
-      <div key={index}>
+      <div className='back' key={index}>
         <p>Name: {holidayItem.name}</p>
         <p>Type: {holidayItem.type}</p>
         <p>Location: {holidayItem.location}</p>
@@ -94,7 +101,7 @@ const handleClickSubmit = () =>{
         <hr />
       </div>
     ))}
-    <button className='but' onClick={handleClickHome}>
+    <button className='nut' onClick={handleClickHome}>
       Home page
     </button>
   </div>
